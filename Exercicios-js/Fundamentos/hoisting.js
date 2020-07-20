652fe348e59ae7b5739ae34bd6a6dacfdae0c820
Esdras Yan <esdrasyan2008@gmail.com>
2020-07-19 22:05:40 -0300

    Adicionando fundamentos de variáveis, let, objeto, hoisting, loops, par nome e valor e atribuição.

---
+++ Exercicios-js/Fundamentos/hoisting.js
@@ -0,0 +1,15 @@
+console.log('a =', a) //Não usar esse tipo de método nos códigos, não tem necessidade e dificulta o entendimento
+var a = 2
+console.log('a =',a)
+
+
+console.log('b =', b)
+let b = 2
+console.log('b =', b)
+
+//outro exemplo
+/*function teste (){
+    console.log('a =', a)
+    var a = 2
+    console.log('a =',a)
+}*/
